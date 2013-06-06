f319294d29bf0385fe204f336527258094471eee
KertarTheDev <kertar@hotmail.com>
2013-06-06 23:38:07 +0200

    Added pseudo-random number generator initialisation.

--- FallingCircles/main.cpp
+++ FallingCircles/main.cpp
@@ -1,9 +1,13 @@
 #include "fcMainWindow.h"
 #include <QApplication>
+#include <QDateTime>
 
 //==========================================================================
 int main(int argc, char *argv[])
 {
+    //initialise pseudo-random number generator
+    qsrand(QDateTime::currentMSecsSinceEpoch());
+
     QApplication a(argc, argv);
     CfcMainWindow w;
     w.show();
