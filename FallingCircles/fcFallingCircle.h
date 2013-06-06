36ba1d5644f2fca3911d8e10fc00e87b42193aa6
KertarTheDev <kertar@hotmail.com>
2013-06-06 21:55:51 +0200

    Issue #5 > Added mPosition property to CfcFallingObject - top left corner coordinates of objects bounding box.

--- FallingCircles/fcFallingCircle.h
+++ FallingCircles/fcFallingCircle.h
@@ -7,8 +7,8 @@ class CfcFallingCircle : public CfcFallingObject
 public:
     CfcFallingCircle(): CfcFallingObject(), mRadius(1.0){}
 
-    CfcFallingCircle(float speed, int scoreValue, float radius)
-        : CfcFallingObject(speed, scoreValue), mRadius(radius){}
+    CfcFallingCircle(QPointF position, float speed, int scoreValue, float radius)
+        : CfcFallingObject(position, speed, scoreValue), mRadius(radius){}
 
 protected:
     float mRadius;
--- FallingCircles/fcFallingObject.h
+++ FallingCircles/fcFallingObject.h
@@ -1,13 +1,18 @@
 #pragma once
 
+#include <QPoint>
+
 class CfcFallingObject
 {
 
 public:
-    CfcFallingObject(): mFallSpeed(1.0), mScorePoints(1){}
+    CfcFallingObject(): mPosition(0,0), mFallSpeed(1.0), mScorePoints(1){}
 
-    CfcFallingObject(float speed, int scoreValue)
-        :mFallSpeed(speed), mScorePoints(scoreValue){}
+    CfcFallingObject(QPointF position, float speed, int scoreValue)
+        : mPosition(position), mFallSpeed(speed), mScorePoints(scoreValue){}
+
+    QPointF position() const {return this->mPosition;}
+    void setPosition(QPointF position) {this->mPosition = position;}
 
     float fallSpeed() const {return this->mFallSpeed;}
     void setFallSpeed(float speed) {this->mFallSpeed = speed;}
@@ -16,6 +21,7 @@ public:
     void setScorePoints(int scoreValue){this->mScorePoints = scoreValue;}
 
 protected:
+    QPointF mPosition;   //coordinates of top left corner of the objects bounding box
     float mFallSpeed;
     int mScorePoints;
 };
