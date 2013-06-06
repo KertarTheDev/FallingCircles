0cb6da4600a0577d27882edc6cda54f8808bde8f
KertarTheDev <kertar@hotmail.com>
2013-06-06 23:15:25 +0200

    Issue #5 > Added forgotten getter and setter to circle's radius.

--- FallingCircles/fcFallingCircle.h
+++ FallingCircles/fcFallingCircle.h
@@ -10,6 +10,9 @@ public:
     CfcFallingCircle(QPointF position, float speed, int scoreValue, float radius)
         : CfcFallingObject(position, speed, scoreValue), mRadius(radius){}
 
+    float radius() const {return this->mRadius;}
+    void setRadius(float radius) {this->mRadius = radius;}
+
 protected:
     float mRadius;
 };
